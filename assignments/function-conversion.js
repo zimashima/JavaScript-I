931d286cc7815444d9d55adb66921f75641de431
Mashima <52222646+zimashima@users.noreply.github.com>
2019-10-14 16:47:21 -0400

    done with everything, plus stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,42 @@
 // };
 // myFunction();
 
+const myFunction = () =>{
+    console.log('Function was invoked!')
+};
+
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+const anotherFunction = (param) => {
+    return param;
+};
+
+console.log(anotherFunction("Example"));
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) => {
+    return param1 + param2;
+};
+
+console.log(add(20,30));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const substract = (param1, param2) => {
+    return param1 - param2;
+};
+
+console.log(substract(50,25));
 
 
 // Stretch
@@ -28,3 +50,10 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+theArray = [5,4,3,2,1]
+
+const triple = theArray.map((num) => {
+    return num *3;
+});
+console.log(triple)
